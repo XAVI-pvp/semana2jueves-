6178fb1cfa4be07a7e67265e15e8400c30dc8cb4
XAVI-pvp <victorjaviervasquez04199@gmal.com>
2026-03-21 21:56:50 -0600

    corrijiendo nombres de arhivos borrados

--- Ejercicios con string semana 9/ejrcicio2.py
+++
@@ -1,4 +0,0 @@
-frase = input("Escribe una frase cualquiera: ")
-print("Frase original: {}".format(frase))
-print("Frase en mayúsculas: {}".format(frase.upper()))
-print("Frase en minúsculas: {}".format(frase.lower()))
