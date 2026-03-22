b79883f535eaf7586fc41effdb994452ce317d4d
XAVI-pvp <victorjaviervasquez04199@gmal.com>
2026-03-21 21:09:08 -0600

    jerccio1 de la semana9

--- Ejercicios con string semana 9/ejercicio1.py
+++
@@ -1,4 +0,0 @@
-nombre_completo = input("Por favor, ingresa tu nombre completo: ")
-print("En mayúsculas: {}".format(nombre_completo.upper()))
-print("En minúsculas: {}".format(nombre_completo.lower()))
-print("Cantidad de caracteres: {}".format(len(nombre_completo)))
