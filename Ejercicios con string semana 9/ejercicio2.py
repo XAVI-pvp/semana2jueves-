29b68f6ccb2365f4950156323d9ef9c9189ece2e
XAVI-pvp <victorjaviervasquez04199@gmal.com>
2026-03-21 21:41:20 -0600

    ejercicio2 semana9

---
+++ Ejercicios con string semana 9/ejercicio2.py
@@ -0,0 +1,4 @@
+frase = input("Escribe una frase cualquiera: ")
+print("Frase original: {}".format(frase))
+print("Frase en mayúsculas: {}".format(frase.upper()))
+print("Frase en minúsculas: {}".format(frase.lower()))
