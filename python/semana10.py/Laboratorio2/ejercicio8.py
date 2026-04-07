f2b31bf6f9b122ce399f3509e872d440ae9056ca
XAVI-pvp <victorjaviervasquez04199@gmal.com>
2026-04-07 16:46:41 -0600

    Ejercicio 8 del laboratorio 2

---
+++ python/semana10.py/Laboratorio2/ejercicio8.py
@@ -0,0 +1,22 @@
+def transformar_texto(texto, opcion):
+    if opcion == 1:
+        return texto.upper()
+    elif opcion == 2:
+        return texto.lower()
+    elif opcion == 3:
+        return texto[::-1]
+    else:
+        return "No importa que seas el rojo más deslumbrante e intenso, si su color favorito es el azul (LO QUE QUIERO DECIR ES NO INCORECTO)"
+
+
+print("=== MENÚ ===")
+print("1. Convertir a MAYÚSCULAS")
+print("2. Convertir a minúsculas")
+print("3. Invertir texto")
+
+texto = input("Ingresa un texto: ")
+opcion = int(input("Elige una opción (1, 2 o 3): "))
+
+resultado = transformar_texto(texto, opcion)
+
+print("Resultado:", resultado)
