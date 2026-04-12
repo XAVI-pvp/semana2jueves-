cb8867c052cfd2f97db75a966abee84a071ecd35
XAVI-pvp <victorjaviervasquez04199@gmal.com>
2026-04-11 23:07:56 -0600

    ejercicio11 Toma la cadena el nido matinal Limpia los espacios en blanco de los extremos y haz que la primera letra de cada palabra pase a ser mayuscula

---
+++ parcial2/ejercicio11.py
@@ -0,0 +1,6 @@
+texto = "  el nido matinal  "
+texto_limpio = texto.strip().title()
+texto_final = texto_limpio.center(30, "-")
+print("Texto original:", texto)
+print("Texto procesado:", texto_limpio)
+print("Resultado final:", texto_final)
