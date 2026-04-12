fd513ec4d1e1a0921143a3eb2cc589a3b2a9e12f
XAVI-pvp <victorjaviervasquez04199@gmal.com>
2026-04-12 00:15:29 -0600

    Ejercico 9 Toma la cadena Any time y Anytime

---
+++ python/semana10.py/parcial2/ejercicio9.py
@@ -0,0 +1,6 @@
+texto = "Any time y Anytime"
+texto_reemplazado = texto.replace("Any time", "Always")
+texto_final = texto_reemplazado.upper()
+print("Texto original:", texto)
+print("Texto reemplazado:", texto_reemplazado)
+print("Resultado final:", texto_final)
