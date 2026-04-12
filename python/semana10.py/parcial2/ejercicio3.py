5cc6a7067739a6ff7638d5cbd7af493181cbb311
XAVI-pvp <victorjaviervasquez04199@gmal.com>
2026-04-12 00:10:14 -0600

    Ejercico 3 Crea una variable con el texto ING Su nombre

---
+++ python/semana10.py/parcial2/ejercicio3.py
@@ -0,0 +1,6 @@
+texto = "ING. victor"
+texto_sin_prefijo = texto.replace("ING. ", "")
+texto_final = texto_sin_prefijo.upper()
+print("Texto original:", texto)
+print("Sin prefijo:", texto_sin_prefijo)
+print("Resultado final:", texto_final)
