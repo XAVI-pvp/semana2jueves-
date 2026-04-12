0bd471037b836baf7bb2e218e6d74f090f9ab02a
XAVI-pvp <victorjaviervasquez04199@gmal.com>
2026-04-11 22:52:09 -0600

    ejercicio1 Declara una variable con la cadena elefante

---
+++ parcial2/Ejercicio1.py
@@ -0,0 +1,6 @@
+texto = "  elefante  "
+texto_limpio = texto.strip()
+cantidad_e = texto_limpio.count("e")
+print("Texto original:", texto)
+print("Texto limpio:", texto_limpio)
+print("Cantidad de 'e':", cantidad_e)
