544537d9bbfb623c6f7e9596d274413f71cb7fb8
XAVI-pvp <victorjaviervasquez04199@gmal.com>
2026-04-11 22:53:07 -0600

    ejercicio2 Toma la cadena de texto Su nombre

---
+++ parcial2/ejercicio2.py
@@ -0,0 +1,7 @@
+texto = "victor javier vasquez ceron"
+texto_mayus = texto.title()
+partes = texto_mayus.split()
+resultado_final = partes[0] + " Vasquez"
+print("Texto original:", texto)
+print("Texto con mayúsculas:", texto_mayus)
+print("Resultado final:", resultado_final)
